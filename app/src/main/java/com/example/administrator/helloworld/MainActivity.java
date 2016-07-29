92b17794532af6017a04bfe284978ab4210baa82
W@M <waninoko67@gmail.com>
2016-07-29 16:25:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.administrator.helloworld;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t = (TextView) findViewById(R.id.textView);
+        t.setText("M");
+        Button a = (Button) findViewById(R.id.ChangeButton);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Hello");
+                builder.setPositiveButton("hi", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"LOL",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("bye", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        Button b = (Button)findViewById(R.id.showtoastbutton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"LOL",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button c = (Button)findViewById(R.id.existbutton);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
